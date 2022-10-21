6db8805bb58ec5a9058ae98e820f8aa7995705a9
lixuling <antonisaputra049@gmail.com>
2022-10-21 16:48:15 +0700

    fix bug

--- src/components/DownloadCV.js
+++ src/components/DownloadCV.js
@@ -3,7 +3,7 @@ import React from 'react'
 const DownloadCV = () => {
   return (
     <div className='fixed h-[40px] w-[120px] bg-icon bottom-2 right-2 flex items-center justify-center text-white font-semibold rounded-full shadow-md shadow-slate-900'>
-      <a href="">Download CV</a>
+      <a href="https://download-server-img.herokuapp.com/download">Download CV</a>
     </div>
   )
 }
